8828887a31108455e62b7272ea88d78d162b5ec1
NamrataSanger <namratasanger55@gmail.com>
2020-09-07 23:23:12 +0530

    Modfication - ['home.js']

--- src/components/main/home.js
+++ src/components/main/home.js
@@ -9,32 +9,23 @@ export default class Home extends Component {
         return (
             <div>
                     <img className="downWave" src={wavedown} alt="Down wave"/>
-                        <div className="container">
                             <div className="leftContainer">
-                                <div className="title">
-                                    <span className="title1">STRATERGIC </span>
-                                </div>
-                                <div className="title">
+                                <span className="title1">STRATERGIC </span>
                                 <span className="title2">HOSPITAL</span>
-                                </div>
                                 <span className="title3">MANAGEMENT</span>
                                 <span className="title4">SYSTEM</span> 
                             </div>
                             <div className="rightContainer">
                                 <div className="box1"></div>
-                                <div className="box2"> 
-                                </div>
-                                <div className="box3">
-                                </div>
+                                <div className="box2"></div>
+                                <div className="box3"></div>
                                 <div className="button">
                                         <span className="text"> Login </span>
                                 </div>
-                                <div className="box4">
-                                </div>
-                                <img className="logo" src={logo} alt="Logo"/>
+                                <div className="box4"></div>
+                                        <img className="logo" src={logo} alt="Logo"/>
                             </div>
-                        </div>
-                    <img className="upWave" src={waveup} alt="Up wave"/>
+                    <img className="upWave" src={waveup} alt="Up wave"/>                  
             </div>
         )
     }
