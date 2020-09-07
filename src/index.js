cbd3fbb9e61e8f8d46c57037c5640f47f3336451
Namrata Sanger <35221403+Namratasanger@users.noreply.github.com>
2020-09-07 08:53:39 +0530

    Added Login module template.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import LandingPage from './component/login/LandingPage.js'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <LandingPage/>
   </React.StrictMode>,
   document.getElementById('root')
 );
