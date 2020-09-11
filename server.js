2406431402c2439861441a964c7995df85c1d0de
NamrataSanger <namratasanger55@gmail.com>
2020-09-11 21:19:46 +0530

    Created Procfile and server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const {createServer} = require("http");
+const express = require("express");
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname,'build')))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'build','index.html'));
+})
+
+const server = createServer(app);
+server.listen(PORT,err =>{
+    if(err) throw err;
+    console.log("Server Started!")
+})
