7508ae182475697d355ca07c2397832f9a32e86e
NamrataSanger <namratasanger55@gmail.com>
2020-09-11 22:13:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const {createServer} = require("http");
 const express = require("express");
 const path = require("path");
@@ -17,24 +16,4 @@ const server = createServer(app);
 server.listen(port,err =>{
     if(err) throw err;
     console.log("Server Started!")
-=======
-const {createServer} = require("http");
-const express = require("express");
-const path = require("path");
-
-const port = process.env.PORT || 3000;
-
-const app = express()
-
-app.use(express.static(path.resolve(__dirname,'build')))
-
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'build','index.html'));
-})
-
-const server = createServer(app);
-server.listen(port,err =>{
-    if(err) throw err;
-    console.log("Server Started!")
->>>>>>> f3a7acc9258ea8be8152b6ba15c47f758d9970e0
 })
